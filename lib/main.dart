6215456f672ece9a03266d3519e0df780b8ab05d
Trevor Knutson <trevor.a.knutson@ndsu.edu>
2023-06-16 16:18:13 -0500

    Changed countdown text style

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'dart:async';
-
 import 'package:flutter/material.dart';
 
 void main() {
@@ -25,15 +24,6 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -80,10 +70,8 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Text(
-              '$_start',
-              style: Theme.of(context).textTheme.headlineLarge,
-            ),
+            Text('$_start', style: const TextStyle(fontSize: 42.0, fontWeight: FontWeight.bold)),
+            const SizedBox(height: 200.0,),
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
