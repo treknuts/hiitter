d6a1cdc5ffcf67c95ea3efa41ef11c6111d2c4fc
Trevor Knutson <trevor.a.knutson@ndsu.edu>
2023-06-23 13:56:07 -0500

    Moved time controls back to the widget to avoid state management stuff

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(const MyApp());
@@ -17,27 +18,20 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.blueGrey),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'HIITTER'),
+      home: const Home(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+class Home extends StatelessWidget {
+  const Home({super.key});
 
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
+        title: const Text("HIITTER"),
       ),
       body: const Center(
         child: IntervalCard(start: 5, title: "Run"),
@@ -94,11 +88,9 @@ class _IntervalCard extends State<IntervalCard> {
   @override
   Widget build(BuildContext context) {
     return Card(
-        elevation: 1,
-        color: Theme.of(context).colorScheme.surfaceVariant,
-        child: SizedBox(
-          width: 300,
-          height: 300,
+        elevation: 0.75,
+        child: ConstrainedBox(
+          constraints: const BoxConstraints.expand(height: 220.0),
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
@@ -110,32 +102,77 @@ class _IntervalCard extends State<IntervalCard> {
                           fontSize: 42.0, fontWeight: FontWeight.bold)),
               Row(
                 mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisSize: MainAxisSize.max,
                 children: [
                   Center(
-                    child: ButtonBar(
-                      children: <Widget>[
-                        IconButton(
-                          onPressed: _startTimer,
-                          icon: const Icon(Icons.play_circle_outline_rounded),
-                          iconSize: 64.0,
-                        ),
-                        IconButton(
-                          onPressed: _stopTimer,
-                          icon: const Icon(Icons.pause_circle_outline_rounded),
-                          iconSize: 64.0,
-                        ),
-                        IconButton(
-                          onPressed: _resetTimer,
-                          icon: const Icon(Icons.restart_alt_rounded),
-                          iconSize: 64.0,
-                        )
-                      ],
-                    ),
-                  ),
+                      child: ButtonBar(
+                    alignment: MainAxisAlignment.center,
+                    mainAxisSize: MainAxisSize.max,
+                    children: [
+                      IconButton(
+                        onPressed: _startTimer,
+                        icon: const Icon(Icons.play_arrow_rounded),
+                        iconSize: 62.0,
+                      ),
+                      IconButton(
+                        onPressed: _stopTimer,
+                        icon: const Icon(Icons.pause),
+                        iconSize: 62.0,
+                      ),
+                      IconButton(
+                        onPressed: _resetTimer,
+                        icon: const Icon(Icons.restore_outlined),
+                        iconSize: 62.0,
+                      )
+                    ],
+                  ))
                 ],
               )
             ],
           ),
         ));
+  }
+}
+
+class MyNavigationBar extends StatefulWidget {
+  const MyNavigationBar({super.key});
+
+  @override
+  State<MyNavigationBar> createState() => _MyNavigationBar();
+}
+
+class _MyNavigationBar extends State<MyNavigationBar> {
+  int _selectedIndex = 0;
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomNavigationBar(
+      items: const <BottomNavigationBarItem>[
+        BottomNavigationBarItem(
+          icon: Icon(Icons.play_circle_fill_rounded),
+          label: 'Play',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.pause_circle_outline_rounded),
+          label: 'Pause',
+          backgroundColor: Colors.green,
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.restart_alt_rounded),
+          label: 'Restart',
+          backgroundColor: Colors.purple,
+        ),
+      ],
+      currentIndex: _selectedIndex,
+      selectedItemColor: Colors.amber[800],
+      onTap: _onItemTapped,
+    );
   }
 }
