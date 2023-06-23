c0c8bbe3f1dfc50741260f73714b3bed1efd5b05
Trevor Knutson <trevor.a.knutson@ndsu.edu>
2023-06-23 11:06:04 -0500

    Moved card into it's own stateful widget.

--- lib/main.dart
+++ lib/main.dart
@@ -32,20 +32,52 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _start = 10; 
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: const Center(
+        child: IntervalCard(start: 5, title: "Run"),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
+class IntervalCard extends StatefulWidget {
+  final int start;
+  final String title;
+  const IntervalCard({Key? key, required this.title, required this.start})
+      : super(key: key);
+
+  @override
+  State<IntervalCard> createState() => _IntervalCard();
+}
+
+class _IntervalCard extends State<IntervalCard> {
+  late int _originalStart;
+  late int _start;
   late Timer _timer;
 
+  @override
+  void initState() {
+    super.initState();
+    _originalStart = widget.start;
+    _start = widget.start;
+  }
+
   void _startTimer() {
-      _timer = Timer.periodic(
-        const Duration(seconds: 1), 
+    _timer = Timer.periodic(
+        const Duration(seconds: 1),
         (timer) => setState(() {
-          if(_start < 1) {
-            timer.cancel();
-          } else {
-            _start--;
-          }
-        })
-        );
+              if (_start < 1) {
+                timer.cancel();
+              } else {
+                _start--;
+              }
+            }));
   }
 
   void _stopTimer() {
@@ -55,41 +87,55 @@ class _MyHomePageState extends State<MyHomePage> {
   void _resetTimer() {
     _timer.cancel();
     setState(() {
-      _start = 10;
+      _start = _originalStart;
     });
   }
-  
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text('$_start', style: const TextStyle(fontSize: 42.0, fontWeight: FontWeight.bold)),
-            const SizedBox(height: 200.0,),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Center(
-                  child: ButtonBar(
-                    children: <Widget>[
-                      IconButton(onPressed: _startTimer, icon: const Icon(Icons.play_circle_outline_rounded), iconSize: 64.0,),
-                      IconButton(onPressed: _stopTimer, icon: const Icon(Icons.pause_circle_outline_rounded), iconSize: 64.0,),
-                      IconButton(onPressed: _resetTimer, icon: const Icon(Icons.restart_alt_rounded), iconSize: 64.0,)
-                    ],
+    return Card(
+        elevation: 1,
+        color: Theme.of(context).colorScheme.surfaceVariant,
+        child: SizedBox(
+          width: 300,
+          height: 300,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(widget.title, style: const TextStyle(fontSize: 42.0)),
+              _start == 0
+                  ? const Text("Done", style: TextStyle(fontSize: 42.0))
+                  : Text('$_start',
+                      style: const TextStyle(
+                          fontSize: 42.0, fontWeight: FontWeight.bold)),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Center(
+                    child: ButtonBar(
+                      children: <Widget>[
+                        IconButton(
+                          onPressed: _startTimer,
+                          icon: const Icon(Icons.play_circle_outline_rounded),
+                          iconSize: 64.0,
+                        ),
+                        IconButton(
+                          onPressed: _stopTimer,
+                          icon: const Icon(Icons.pause_circle_outline_rounded),
+                          iconSize: 64.0,
+                        ),
+                        IconButton(
+                          onPressed: _resetTimer,
+                          icon: const Icon(Icons.restart_alt_rounded),
+                          iconSize: 64.0,
+                        )
+                      ],
+                    ),
                   ),
-                )
-              ],
-            )
-            
-          ],
-        ),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+                ],
+              )
+            ],
+          ),
+        ));
   }
 }
